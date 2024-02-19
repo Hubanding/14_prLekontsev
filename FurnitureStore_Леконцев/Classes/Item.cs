bea9523f85cce86aa43c91243e35db202f0d2c35
vanal <vanalekoncev@gmail.com>
2024-02-20 02:22:42 +0500

    Разработанный класс. Леконцев».

---
+++ FurnitureStore_Леконцев/Classes/Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurnitureStore_Леконцев.Classes
+{
+    internal class Item
+    {
+        public string name { get; set; }
+        public int price { get; set; }
+        public string src { get; set; }
+
+        public int activeSize = 0;
+
+        public Item(string name, int price, string src)
+        {
+            this.name = name;
+            this.price = price;
+            this.src = src;
+        }
+    }
+}
